bd7a5a92d9944f0822eda7b0f7ddf7cbff07e3f4
WebDev1119 <warriorjhs@outlook.com>
2020-03-26 22:25:56 +0800

    add photo

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,18 @@
 import React from "react";
 
 export default () => {
+  const photos = [
+    {
+      src: "http://example.com/example/img1.jpg",
+      width: 4,
+      height: 3
+    },
+    {
+      src: "http://example.com/example/img2.jpg",
+      width: 1,
+      height: 1
+    }
+  ];
   return (
     <div>
       <h3 className="text-orange-600">Welcome! Sign up or Sign in!</h3>
